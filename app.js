4234e859c2346c86c24f3aaa543c9c7ad5dc7cb9
FELIPE DE LIMA RODRIGUES <felipe_l_rodrigues@estudante.sc.senai.br>
2025-05-12 21:45:19 -0300

    criando css e js

---
+++ app.js
@@ -0,0 +1,40 @@
+const db = new PouchDB('Finances');
+const form = document.getElementById('RecordForm');
+const list = document.getElementById('RecordsList');
+const TotalDaily = document.getElementById('DailySum');
+const TotalSum = document.getElementById('TotalSum');
+const today = new Date().toISOString().slice(0,10);
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const desc = document.getElementById('Description').value;
+    const value = document.getElementById('Value').value;
+    const recordtype = document.getElementById('Type').value;
+    const date = today;
+
+    const doc = {
+        _id: new new Date().toISOString(),
+        desc, value, recordtype, date
+    };
+
+    await db.put(doc);
+    form.reset();
+    refresh();
+})
+
+async function refresh() {
+    const res = await db.allDocs({include_docs: true, descending: true});
+    list.innerHTML = '';
+    let totalD = 0;
+    let totalG = 0;
+
+    res.rows.forEach(row => {
+        const {desc, value, recordtype, date} = row.doc;
+        const li = document.createElement('li');
+        li.textContent = `${date} - ${desc} : R$ ${value.toFixed(2)} - (${recordTyoe})`
+        list.appendChild(li);
+});
+
+}
+
+refresh();
